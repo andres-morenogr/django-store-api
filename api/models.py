f303e0e44e1f9420065f39d4ed1f22a702f2abc2
kevinmaiden7 <kevin.m.gallego@gmail.com>
2020-11-29 12:43:28 -0500

    Fixing model

--- api/models.py
+++ api/models.py
@@ -1,49 +1,19 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
-
-# modelo vendedor
-class Seller(models.Model):
-    name = models.CharField(max_length=100)
-    logo = models.URLField(max_length = 200)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = 'sellers'
-        verbose_name = 'Seller'
-        verbose_name_plural = 'Sellers'
-        ordering = ['id']
-
-# modelo ciudad
-class City(models.Model):
-    name = models.CharField(max_length=100)
-    code = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = 'cities'
-        verbose_name = 'City'
-        verbose_name_plural = 'Cities'
-        ordering = ['id']
-
+#from django.contrib.postgres.fields import ArrayField
 
 # modelo item
 class Item(models.Model):
     name = models.CharField(max_length=300)
     brand = models.CharField(max_length=300)
     thumbnail = models.URLField(max_length = 200)
-    pictures = ArrayField(models.URLField(max_length = 200))
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    pictures = models.CharField(max_length = 200)
+    city = models.CharField(max_length = 200)
+    seller = models.CharField(max_length = 200)
     description = models.TextField(max_length=1000, verbose_name='Información del item')
     price = models.FloatField()
     currency = models.CharField(max_length=5)
     rating = models.IntegerField()
     
-
     def __str__(self):
         return self.name
 
